b286691a4e25e01dbe492db624e4d36aa4905548
ThejusKTBFFH <1thejusjoshi@gmail.com>
2023-02-07 16:08:12 +0530

    added subcomponent for home route

--- src/Display/Home.jsx
+++ src/Display/Home.jsx
@@ -1,10 +1,30 @@
 import React from 'react'
 import SideBar from '../SideBar/sidebar'
+import Property from './Property'
 
 export default function Home(){
     return(
+        <>
+        
+        <div className='home'>
+      
+      <section className='one'>
+       <SideBar/>
+      </section>
+      
+      <section className='two'>
+      <Property />
+      {/* <SearchAdd /> */}
+      {/* <ListProp/> */}
+      </section>
         <div>
-            <SideBar/>
         </div>
+    </div>
+        
+        
+        </>
+    
+         
+      
     )
 }
