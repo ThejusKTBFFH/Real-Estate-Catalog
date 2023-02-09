fe2e0da445663a5fdd5a329ac3bca94d1f3740e2
Devendra Gupta <guptadevendra7432@gmail.com>
2023-02-09 16:48:05 +0530

    add new property and basicinfo route added

--- src/Display/Home.jsx
+++ src/Display/Home.jsx
@@ -5,25 +5,24 @@ import Property from './Property'
 export default function Home(){
     return(
         <>
-        
-        <div className='home'>
-      
-      <section className='one'>
-       <SideBar/>
-      </section>
-      
-      <section className='two'>
-      <Property />
-      {/* <SearchAdd /> */}
-      {/* <ListProp/> */}
-      </section>
-        <div>
-        </div>
-    </div>
-        
-        
+
+            <div className='home'>
+
+                <section className='one'>
+                    <SideBar />
+                </section>
+
+                <section className='two'>
+                    <Property />
+                    {/* <SearchAdd /> */}
+                    {/* <ListProp/> */}
+                </section>
+              
+            </div>
+
+
         </>
-    
+
          
       
     )
