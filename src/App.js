64880518c6cff53be3d804c49f012cd4bc9644b8
Devendra Gupta <guptadevendra7432@gmail.com>
2023-02-10 00:59:23 +0530

    location info added and made some changes to other routes

--- src/App.js
+++ src/App.js
@@ -7,39 +7,24 @@ import LocationInfo from './components/LocationInfo';
 import PropertyDetail from './components/PropertyDetails';
 import GeneralInfo from './components/Generalinfo';
 
-import {BrowserRouter,Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    
-    <BrowserRouter>
+   return (
 
-    <Routes>
-    <Route path="/" element={<Signin />} />
-    <Route path="/signup" element={<Signup/>}/>
-    <Route path="/home" element={<Home/>}/>
-   <Route path='/basicinfo' element={<BasicInfo/>}/>
-   <Route path='/locationinfo' element={<LocationInfo/>}/>
+      <BrowserRouter>
 
-   <Route path="/propertyDetail" element={<PropertyDetail/>}/>
-   <Route path="/generalinfo" element={<GeneralInfo/>}/>
+         <Routes>
+            <Route path="/" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/home" element={<Home />} />
+            <Route path='/basicinfo' element={<BasicInfo />} />
+            <Route path="/propertyDetail" element={<PropertyDetail />} />
+            <Route path="/generalinfo" element={<GeneralInfo />} />
+            <Route path='/locationinfo' element={<LocationInfo />} />
+         </Routes>
 
-    </Routes>
-    
-    
-
-    
-    
-    
-    </BrowserRouter>
-
-
-      
-
-
-      
-      
-   
+      </BrowserRouter>
   );
 }
 
--- src/components/LocationInfo.jsx
+++ src/components/LocationInfo.jsx
@@ -6,23 +6,23 @@ import { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./LocationInfo.css";
 
-export default function LocationInfo(){
-    const navigator = useNavigate()
-    const location = useLocation()
-    const [locationInfo, setLocationInfo] = useState({
-        email: "",
-        city: "",
-        area: "",
-        pincode: "",
-        address: "",
-        landmark: "",
-        latitude: "",
-        longitude: "",
-      });
-       
-    return(
-        <>
-        <div className="home">
+export default function LocationInfo() {
+  const navigator = useNavigate()
+  const location = useLocation()
+  const [locationInfo, setLocationInfo] = useState({
+    email: "",
+    city: "",
+    area: "",
+    pincode: "",
+    address: "",
+    landmark: "",
+    latitude: "",
+    longitude: "",
+  });
+
+  return (
+    <>
+      <div className="home">
         <div className="one">
           <Sidebar />
         </div>
@@ -31,169 +31,169 @@ export default function LocationInfo(){
           <AddNewProperty />
 
           <div className="maincontainer">
-          <div className="box1">
-            <div className="row">
-              <div>
-                <span>Email</span>
+            <div className="box1">
+              <div className="row">
+                <div>
+                  <span>Email</span>
 
-                <div className="selectBox">
-                  <input
-                    className="selectBox"
-                    placeholder="Email"
-                    onChange={(e) => {
-                      setLocationInfo({
-                        ...LocationInfo,
-                        email: e.target.value,
-                      });
-                    }}
-                  />
+                  <div className="selectBox">
+                    <input
+                      className="selectBox"
+                      placeholder="Email"
+                      onChange={(e) => {
+                        setLocationInfo({
+                          ...LocationInfo,
+                          email: e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <span>City</span>
+                  <div className="selectBox">
+                    <select
+                      defaultValue={"toilet"}
+                      className="selectBox"
+                      onChange={(e) => {
+                        setLocationInfo({
+                          ...LocationInfo,
+                          city: e.target.value,
+                        });
+                      }}
+                    >
+                      <option value={"toilet"}>Select City</option>
+                      <option>Delhi</option>
+                      <option>Ranchi</option>
+                      <option>Mumbai</option>
+                      <option>Bangalore</option>
+                    </select>
+                  </div>
                 </div>
               </div>
 
-              <div>
-                <span>City</span>
-                <div className="selectBox">
-                  <select
-                    defaultValue={"toilet"}
-                    className="selectBox"
-                    onChange={(e) => {
-                      setLocationInfo({
-                        ...LocationInfo,
-                        city: e.target.value,
-                      });
-                    }}
-                  >
-                    <option value={"toilet"}>Select City</option>
-                    <option>Delhi</option>
-                    <option>Ranchi</option>
-                    <option>Mumbai</option>
-                    <option>Bangalore</option>
-                  </select>
+              <div className="row">
+                <div>
+                  <span> Area</span>
+
+                  <div className="selectBox">
+                    <select
+                      defaultValue={"toilet"}
+                      className="selectBox"
+                      onChange={(e) => {
+                        setLocationInfo({
+                          ...LocationInfo,
+                          area: e.target.value,
+                        });
+                      }}
+                    >
+                      <option value={"toilet"}>Select Area</option>
+                      <option>Yes</option>
+                      <option>NO</option>
+                    </select>
+                  </div>
                 </div>
-              </div>
-            </div>
 
-            <div className="row">
-              <div>
-                <span> Area</span>
-
-                <div className="selectBox">
-                  <select
-                    defaultValue={"toilet"}
-                    className="selectBox"
-                    onChange={(e) => {
-                      setLocationInfo({
-                        ...LocationInfo,
-                        area: e.target.value,
-                      });
-                    }}
-                  >
-                    <option value={"toilet"}>Select Area</option>
-                    <option>Yes</option>
-                    <option>NO</option>
-                  </select>
-                </div>
-              </div>
-
-              <div>
-                <span>Pincode</span>
-                <div className="selectBox">
-                  <select
-                    defaultValue={"toilet"}
-                    className="selectBox"
-                    onChange={(e) => {
-                      setLocationInfo({
-                        ...LocationInfo,
-                        pincode: e.target.value,
-                      });
-                    }}
-                  >
-                    <option value={"toilet"}>Select Pincode</option>
-                    <option>834004</option>
-                    <option>834001</option>
-                  </select>
-                </div>
-              </div>
-            </div>
-</div>
-<div className="box2">
-            <div className="row">
-              <div>
-                <span>Address</span>
-
-                <div className="selectBox">
-                  <input
-                    className="selectBox"
-                    placeholder="Address"
-                    onChange={(e) => {
-                      setLocationInfo({
-                        ...LocationInfo,
-                        address: e.target.value,
-                      });
-                    }}
-                  />
-                </div>
-              </div>
-
-              <div>
-                <span>Landmark</span>
-                <div className="selectBox">
-                  <input
-                    className="selectBox"
-                    placeholder="Landmark"
-                    onChange={(e) => {
-                      setLocationInfo({
-                        ...LocationInfo,
-                        landmark: e.target.value,
-                      });
-                    }}
-                  />
+                <div>
+                  <span>Pincode</span>
+                  <div className="selectBox">
+                    <select
+                      defaultValue={"toilet"}
+                      className="selectBox"
+                      onChange={(e) => {
+                        setLocationInfo({
+                          ...LocationInfo,
+                          pincode: e.target.value,
+                        });
+                      }}
+                    >
+                      <option value={"toilet"}>Select Pincode</option>
+                      <option>834004</option>
+                      <option>834001</option>
+                    </select>
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="box2">
+              <div className="row">
+                <div>
+                  <span>Address</span>
 
-            <div className="row">
-              <div>
-                <span>Latitude</span>
+                  <div className="selectBox">
+                    <input
+                      className="selectBox"
+                      placeholder="Address"
+                      onChange={(e) => {
+                        setLocationInfo({
+                          ...LocationInfo,
+                          address: e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
 
-                <div className="selectBox">
-                  <input
-                    className="selectBox"
-                    placeholder="Latitude"
-                    onChange={(e) => {
-                      setLocationInfo({
-                        ...LocationInfo,
-                        latitude: e.target.value,
-                      });
-                    }}
-                  />
+                <div>
+                  <span>Landmark</span>
+                  <div className="selectBox">
+                    <input
+                      className="selectBox"
+                      placeholder="Landmark"
+                      onChange={(e) => {
+                        setLocationInfo({
+                          ...LocationInfo,
+                          landmark: e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
 
-              <div>
-                <span>Longitude</span>
-                <div className="selectBox">
-                  <input
-                    className="selectBox"
-                    placeholder="Latitude"
-                    onChange={(e) => {
-                      setLocationInfo({
-                        ...LocationInfo,
-                        longitude: e.target.value,
-                      });
-                    }}
-                  />
+              <div className="row">
+                <div>
+                  <span>Latitude</span>
+
+                  <div className="selectBox">
+                    <input
+                      className="selectBox"
+                      placeholder="Latitude"
+                      onChange={(e) => {
+                        setLocationInfo({
+                          ...LocationInfo,
+                          latitude: e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <span>Longitude</span>
+                  <div className="selectBox">
+                    <input
+                      className="selectBox"
+                      placeholder="Latitude"
+                      onChange={(e) => {
+                        setLocationInfo({
+                          ...LocationInfo,
+                          longitude: e.target.value,
+                        });
+                      }}
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-</div>
           <div className="newbuttoncontainer">
             <div className="newbutton">
               <button
-                // onClick={() => {
-                //   navigator("/generalInfo", { state: { generalInfo } });
-                // }}
+                onClick={() => {
+                  navigator("/generalInfo");
+                }}
               >
                 Previous
               </button>
@@ -206,5 +206,5 @@ export default function LocationInfo(){
         </div>
       </div>
     </>
-    )
+  )
 }
